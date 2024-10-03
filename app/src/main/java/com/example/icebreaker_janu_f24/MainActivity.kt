d6982e39713a68637e6d62e509ba05ed6e1e4dbe
jhanu <jvell1@unh.newhaven.edu>
2024-10-03 17:12:41 -0400

    modified git file

--- app/src/main/java/com/example/icebreaker_janu_f24/MainActivity.kt
+++ app/src/main/java/com/example/icebreaker_janu_f24/MainActivity.kt
@@ -1,21 +1,38 @@
 package com.example.icebreaker_janu_f24
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.icebreaker_janu_f24.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSetRandomQuestion.setOnClickListener {
+            binding.txtQuestion.text = "Hello"
+
         }
+        binding.btnSubmit.setOnClickListener {
+            binding.txtQuestion.text = ""
+
+        }
+        private fun writeStudentToFirebase(){
+            val FirstName = binding.Textfirstname
+            val LastName = binding.LastName
+            val prefName = binding.prefName
+        }
+
     }
-}
+
+
+
+
+    }
+
+
 //
---
+++ app/src/main/java/com/example/icebreaker_janu_f24/Questions.kt
@@ -0,0 +1,12 @@
+package com.example.icebreaker_janu_f24
+
+import org.w3c.dom.Text
+
+/**
+ * @property text  the physical text of the question string returned from Firebase
+ */
+data class Questions(var text: String =""){
+
+
+
+}

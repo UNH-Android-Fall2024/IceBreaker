53b0e891e01033cd6d6ca9a7caa0a8cad1748887
jhanu <jvell1@unh.newhaven.edu>
2024-10-08 17:02:04 -0400

    Created App and done all related to the UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -38,12 +40,14 @@ android {
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/icebreaker_janu_f24/MainActivity.kt
+++ app/src/main/java/com/example/icebreaker_janu_f24/MainActivity.kt
@@ -2,37 +2,85 @@ package com.example.icebreaker_janu_f24
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.example.icebreaker_janu_f24.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private val className = "F24"
     private lateinit var binding: ActivityMainBinding
+    private val db = Firebase.firestore
+    private var TAG = "Myapplication-Icetag"
+    private var questionBank: MutableList<Questions>? = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        getQuestionsFromFirebase()
 
         binding.btnSetRandomQuestion.setOnClickListener {
-            binding.txtQuestion.text = "Hello"
-
+            val randomQuestion = questionBank!!.random().text
+            binding.txtQuestion.text = randomQuestion
         }
         binding.btnSubmit.setOnClickListener {
+            writeStudentToFirebase()
             binding.txtQuestion.text = ""
+        }
+    }
+    private fun getQuestionsFromFirebase(){
+        db.collection("Questions")
+            .get()
+            .addOnSuccessListener { result ->
+                questionBank = mutableListOf()
+                for(document in result){
+                    val question = document.toObject(Questions::class.java)
+                    questionBank!!.add(question)
+                    Log.d(TAG,"$question")
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "error", e)
+            }
+    }
+    private fun writeStudentToFirebase() {
+        val firstName = binding.Textfirstname
+        val lastName = binding.LastName
+        val prefName = binding.prefName
+        val answer = binding.textAnswer
 
-        }
-        private fun writeStudentToFirebase(){
-            val FirstName = binding.Textfirstname
-            val LastName = binding.LastName
-            val prefName = binding.prefName
-        }
+        val tag = "Myapplication-Icetag"
+        val msg = "Variables: $firstName $lastName $prefName $answer"
+
+        Log.d(tag, msg)
+        val student = hashMapOf(
+            "firstname" to firstName.text.toString(),
+            "lastname" to lastName.text.toString(),
+            "prefname" to prefName.text.toString(),
+            "answer" to answer.text.toString(),
+            "class" to className,
+            "question" to binding.txtQuestion.text.toString()
+        )
+        val addOnFailureListener = db.collection("students")
+            .add(student)
+            .addOnSuccessListener { documentReference ->
+                Log.d(
+                    TAG,
+                    "Document Snapshot written successfully with ID: ${documentReference.id}"
+                )
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error adding Document", exception)
+            }
+        firstName.setText("")
+        lastName.setText("")
+        prefName.setText("")
+        answer.setText("")
+        binding.txtQuestion.text = ""
 
     }
-
-
-
-
-    }
-
-
-//
+}//
